ed7f39e64b38c5cf9105e50953afceba6d18128e
tony <tony8958@gmail.com>
2019-06-10 06:21:19 +0000

    add wavread

--- capture.c
+++ capture.c
@@ -17,12 +17,12 @@ unsigned int val,val2;
 int dir;
 snd_pcm_uframes_t frames;
 char *buffer;
-if(  (fp =fopen("sound.wav","w")) < 0)
+if((fp =fopen("sound.wav","w")) < 0)
 printf("open sound.wav fial\n");
 /* Open PCM device for recording (capture). */
 rc = snd_pcm_open(&handle, "default",SND_PCM_STREAM_CAPTURE, 0);
 if (rc < 0) {
-fprintf(stderr,"unable to open pcm device: %s/n",  snd_strerror(rc));
+fprintf(stderr,"unable to open pcm device: %s/n",snd_strerror(rc));
 exit(1);
 }
 /* Allocate a hardware parameters object. */
@@ -40,31 +40,31 @@ SND_PCM_FORMAT_S16_LE);
 snd_pcm_hw_params_set_channels(handle, params, 2);
 /* 44100 bits/second sampling rate (CD quality) */
 val = 44100;
-snd_pcm_hw_params_set_rate_near(handle, params,  &val, &dir);
+snd_pcm_hw_params_set_rate_near(handle, params,&val, &dir);
 /* Set period size to 32 frames. */
 frames = 32;
-snd_pcm_hw_params_set_period_size_near(handle,  params, &frames, &dir);
+snd_pcm_hw_params_set_period_size_near(handle,params, &frames, &dir);
 /* Write the parameters to the driver */
 rc = snd_pcm_hw_params(handle, params);
 if (rc < 0) {
-fprintf(stderr,  "unable to set hw parameters: %s/n",
+fprintf(stderr,"unable to set hw parameters: %s/n",
 snd_strerror(rc));
 exit(1);
 }
 /* Use a buffer large enough to hold one period */
-snd_pcm_hw_params_get_period_size(params,  &frames, &dir);
+snd_pcm_hw_params_get_period_size(params,&frames, &dir);
 size = frames * 4; /* 2 bytes/sample, 2 channels */
 printf("size = %d\n",size);
 buffer = (char *) malloc(size);
 /* We want to loop for 5 seconds */
-snd_pcm_hw_params_get_period_time(params,  &val, &dir);
+snd_pcm_hw_params_get_period_time(params,&val, &dir);
 loops = 10000000 / val;
 while (loops > 0) {
 loops--;
 rc = snd_pcm_readi(handle, buffer, frames);
 printf("%d\n",i++);
 if (rc == -EPIPE) {
-/* EPIPE means overrun */  
+/* EPIPE means overrun */
 fprintf(stderr, "overrun occurred/n");
 snd_pcm_prepare(handle);
 } else if (rc < 0) {
@@ -77,7 +77,7 @@ fprintf(stderr, "short read, read %d frames/n", rc);
 //rc = fwrite( buffer,1, size,fp);
 rc = write(1,buffer,size);
 if (rc != size)
-fprintf(stderr,  "short write: wrote %d bytes/n", rc);
+fprintf(stderr,"short write: wrote %d bytes/n", rc);
 else printf("fwrite buffer success\n");
 }
 /******************打印参数*********************/
@@ -126,5 +126,5 @@ snd_pcm_drain(handle);
 snd_pcm_close(handle);
 fclose(fp);
 free(buffer);
-return 0
+return 0;
 }
