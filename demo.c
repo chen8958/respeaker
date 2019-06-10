cab0b43cd888eec313b7063740ac20a84f7c610e
chen8958 <tony8958@gmail.com>
2019-06-10 14:39:19 +0800

    fix Makefile and wav detail

--- demo.c
+++ demo.c
@@ -6,5 +6,17 @@ AudioFile<double> audioFile;
 int main(int argc, char const *argv[]) {
     //system("arecord -Dhw:0,0 -f S16_LE -r 16000 -c 2 -d 1 hello.wav");
     audioFile.load ("./hello.wav");
+    int sampleRate = audioFile.getSampleRate();
+    int bitDepth = audioFile.getBitDepth();
+
+    int numSamples = audioFile.getNumSamplesPerChannel();
+    double lengthInSeconds = audioFile.getLengthInSeconds();
+
+    int numChannels = audioFile.getNumChannels();
+    bool isMono = audioFile.isMono();
+    bool isStereo = audioFile.isStereo();
+
+// or, just use this quick shortcut to print a summary to the console
+    audioFile.printSummary();
     return 0;
 }
