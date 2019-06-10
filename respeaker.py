3a01725c04a430a912f757ca3e21654b59c46083
chen8958 <tony8958@gmail.com>
2019-06-10 21:39:35 +0800

    add respeaker.py das

--- respeaker.py
+++ respeaker.py
@@ -1,16 +1,52 @@
 import wave
-import sys
+import os
 import numpy as np
+from scipy.fftpack import fft,ifft
+import math
+import cmath
 
+def record():
+    os.system("arecord -Dhw:0,0 -f S16_LE -r 16000 -c 6 -d 1 hello.wav");
 def readwav():
-    f=wave.open("hello.wav",'rb');
+    f=wave.open("300.wav",'rb');
     params=f.getparams();
     original_data_string=f.readframes(params[3]);
     original_data=np.fromstring(original_data_string,dtype=np.short);
     original_data.shape=-1,params[0];
     original_data=original_data.T;
     print(original_data.shape);
+    print(params[2]);
+    return original_data,params[2];
+def das(data,fs,MicPos):
+    MicNum = data.shape[0];
+    NWIN=1024;
+    hopsize=NWIN/2;
+    NFFT=1024;
+    df=fs/NFFT;
+    fft_x=np.zeros((data.shape[0],NFFT),dtype=np.complex_);
+    for i in range(MicNum):
+        fft_x[i,:]=fft(data[i,:],1024);
+    print(fft_x);
+    print(fft_x.shape);
+    angel_curve=np.zeros(360);
+    c=343;
+
+    for deg in range(0,359):
+        for ff in range(0,int((NFFT/2)-1)):
+            k=2*math.pi*ff*df/c;
+            kappa=np.array([math.cos(deg/180*math.pi),math.sin(deg/180*math.pi),0]);
+            a=np.zeros((MicNum),dtype=np.complex_);
+            for MicNo in range(0,MicNum-1):
+                a[MicNo]=cmath.exp(complex(0,1)*k*np.dot(kappa,MicPos[:,MicNo]));
+            w=np.conj(a);
+            angel_curve[deg]=angel_curve[deg]+abs(np.dot(w,fft_x[:,ff]));
+    print(np.argmax(angel_curve));
 def main():
-    readwav();
+    record();
+    MicPos=(1/100)*np.array([[4.5*math.cos(120/180*math.pi),4.5*math.cos(60/180*math.pi),4.5,4.5*math.cos(-60/180*math.pi),4.5*math.cos(-120/180*math.pi),-4.5],[4.5*math.sin(120/180*math.pi),4.5*math.sin(60/180*math.pi),0,4.5*math.sin(-60/180*math.pi),4.5*math.sin(-120/180*math.pi),0],[0,0,0,0,0,0]]);
+    print(MicPos);
+    print(MicPos.shape);
+    data,fs=readwav();
+    das(data,fs,MicPos);
 if __name__ == '__main__':
     main();
