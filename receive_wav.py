f6786e8e53d836f309edb9acb1f76500c19a488c
chen8958 <tony8958@gmail.com>
2019-06-13 14:28:06 +0800

    change buff

--- receive_wav.py
+++ receive_wav.py
@@ -35,9 +35,9 @@ class MySocket:
         t.start();
 
 def file(conn,addr):
-    fileinfo_size=struct.calcsize('136sl');
+    fileinfo_size=struct.calcsize('128s8s');
     buf = conn.recv(fileinfo_size);
-    filename,filesize = struct.unpack('136sl',buf);
+    filename,filesize = struct.unpack('128s8s',buf);
     filename_f = filename.decode().strip('\00');
     filename_f = "new_"+filename_f;
     print("file name = {}".format(filename_f));
